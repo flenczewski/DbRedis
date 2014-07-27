cb604f91dea7f6284c15139ea7abcdbcddc89fe5
Fabian <fabian.lenczewski@gmail.com>
2014-07-27 23:16:01 +0200

    fix Singleton Pattern (remove globals)

--- DbRedis.php
+++ DbRedis.php
@@ -22,10 +22,10 @@ class DbRedis
     
     // łączny czas zapytań
     public $queryTime = 0;
-    
+
     // czy włączone debugowanie zapytań
-    private $_debug = false;        // ilcznik ilości i czasu zapytań
-    private $_debugQuery = false;   // podgląd zapytań
+//    private $_debug = false;        // ilcznik ilości i czasu zapytań
+//    private $_debugQuery = false;   // podgląd zapytań
 
     // seperacja kluczy
     const KEY_SEPARATOR = ':';
@@ -35,15 +35,11 @@ class DbRedis
     const CODE_WRONG_PARAMS = -2;   // np. złe parametry
     const CODE_NOT_FOUND    = -3;   // np. nie znaleziono klucza
     //
-    
-    function __construct($config, $namespace)
+
+    private function __construct($config = array(), $namespace = null)
     {
         #$this->_debug = true;
         #$this->_debugQuery = true;
-        
-        if (is_object(self::$_instance[$namespace])) {
-            throw new Exception('DbRedis: Please use method DbRedis::getInstance()');
-        }
 
         try {
             $this->db = new \Redis;
@@ -54,7 +50,7 @@ class DbRedis
 
             $this->isConnecton = true;
         } catch(Exception $e) {
-            throw new Exception( 'DbRedis: '. $e->getMessage() );
+            throw new DbRedisException( $e->getMessage() );
         }
     }
     
@@ -79,20 +75,22 @@ class DbRedis
         }
         $namespace = implode('_', $namespace);
 
-        if( !is_object(self::$_instance[$namespace]) ) {
+
+        if( !isset(self::$_instance[$namespace]) || !(self::$_instance[$namespace] instanceof self) ) {
             self::$_instance[$namespace] =  new self($config, $namespace);
         }
+
         return self::$_instance[$namespace];
     }
     
 
     function __destruct()
     {
-        if($this->_debug) {
-            print_r("\n\n -- ". $this->queryCount .' req. at '. $this->queryTime .' s.');
-        }
+//        if($this->_debug) {
+//            print_r("\n\n -- ". $this->queryCount .' req. at '. $this->queryTime .' s.');
+//        }
 
-        if(isset($this->isConnecton)) {
+        if($this->isConnecton) {
             $this->db->close();
         }
     }
@@ -103,29 +101,29 @@ class DbRedis
             if (method_exists($this->db, $method) ) {
                 
                 // zwiększenie licznika zapytań
-                if($this->_debug) { 
+                //if($this->_debug) {
                     $this->queryCount++;
-                }
+                //}
                 
-                // pomiar czasu - start
-                if($this->_debug) { 
-                }
+//                // pomiar czasu - start
+//                if($this->_debug) {
+//                }
                 
                 // wywołanie funckji
                 $result = call_user_func_array(array($this->db, $method), $args);
                 
-                // pomiar czasu - stop
-                if($this->_debug) { 
-                }
+//                // pomiar czasu - stop
+//                if($this->_debug) {
+//                }
                 
-                // zwiększenie licznika czasu zapytań
-                if($this->_debug) { 
-                }
+//                // zwiększenie licznika czasu zapytań
+//                if($this->_debug) {
+//                }
 
                 // podgląd zapytań
-                if($this->_debug && $this->_debugQuery) { 
-                    $this->_showQuery($queryTime, $method, $args);
-                }
+//                if($this->_debug && $this->_debugQuery) {
+//                    $this->_showQuery($queryTime, $method, $args);
+//                }
                 
                 return $result;
             } else {
@@ -133,49 +131,56 @@ class DbRedis
             }
         } else {
         }
+
+        return true;
     }
     
-    /**
-     * Podgląd zapytań do bazy
-     * 
-     * @param string $queryTime
-     * @param string $method
-     * @param array $args
-     */
-    private function _showQuery($queryTime, $method, $args) 
-    {
-        print_r( $queryTime .'s.' );
-        print_r( $method );
-        print_r( self::_flattenArray($args) );
-    }
+//    /**
+//     * Podgląd zapytań do bazy
+//     *
+//     * @param string $queryTime
+//     * @param string $method
+//     * @param array $args
+//     */
+//    private function _showQuery($queryTime, $method, $args)
+//    {
+//        print_r( $queryTime .'s.' );
+//        print_r( $method );
+//        print_r( self::_flattenArray($args) );
+//    }
     
-    /**
-     * Prasuje tablicę do stringa
-     * 
-     * @param array $array - tablica do prasowania
-     * 
-     * @return string
-     */
-    private static function _flattenArray( $array)
-    {
-        $line = '';
+//    /**
+//     * Prasuje tablicę do stringa
+//     *
+//     * @param array $array - tablica do prasowania
+//     *
+//     * @return string
+//     */
+//    private static function _flattenArray( $array)
+//    {
+//        $line = '';
+//
+//        if ( is_array($array) ) {
+//            $line .= (count($array) > 2) ? ' array(' : '';
+//            foreach ($array as $key => $value) {
+//               $line .= self::_flattenArray($value);
+//               if ($key == count($array)-1 && substr($line, -2) == ', ') {
+//                    $line = substr($line, 0, -2);
+//               }
+//            }
+//            $line .= (count($array) > 2) ? ') ' : '';
+//        } else {
+//             $line = $array . ', ';
+//        }
+//
+//        return $line;
+//    }
 
-        if ( is_array($array) ) {
-            $line .= (count($array) > 2) ? ' array(' : '';
-            foreach ($array as $key => $value) {
-               $line .= self::_flattenArray($value);
-               if ($key == count($array)-1 && substr($line, -2) == ', ') {
-                    $line = substr($line, 0, -2);
-               }
-            }
-            $line .= (count($array) > 2) ? ') ' : '';
-        } else {
-             $line = $array . ', ';
-        }
-        
-        return $line;
-    }     
 
 
+}
+
+
+class DbRedisException extends  Exception {
 
 }
